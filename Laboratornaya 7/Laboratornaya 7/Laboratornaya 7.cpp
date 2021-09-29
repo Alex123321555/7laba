3963578acf5ded4453a6796e49e87b73e76ea8ac
Alex123321555 <48363030+Alex123321555@users.noreply.github.com>
2021-09-30 00:12:52 +0200

    Add files via upload

---
+++ Laboratornaya 7/Laboratornaya 7/Laboratornaya 7.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+// 13.Найти минимум из сумм элементов строк
+
+int main()
+{
+    int A[100][100];
+    int n;
+    int m;
+    int min = 99999999;
+
+    cout << "Vvedite kol-vo strok" << endl;
+    cin >> n;
+    cout << "Vvedite kol-vo stolbcov" << endl;
+    cin >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Vvedite elementy " << i + 1 << " stroki" << endl;
+        for (int j = 0; j < m; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += A[i][j];
+        }
+
+        if (sum < min)
+            min = sum;
+    }
+    cout << "Minimum = " << min << endl;
+
+    system("pause");
+}
+
